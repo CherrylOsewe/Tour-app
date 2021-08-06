9b81a0646bd50022244459a20da1613f1f7c7e1d
FREDRICK GACHURU MWANGI <fredrickgachuru73@gmail.com>
2021-08-06 03:37:21 +0300

    merge (#16)
    * Ft homepage (#9)
    
    * Initial commit
    
    * Add header and paragraph
    
    * Style homepage
    
    * Add new page
    
    * Add images
    
    * Add logo
    
    * Add navbar and countries section
    
    * Style homepage:
    
    * Modify Homepage
    
    * Add footer and contact section
    
    * Initial commit
    
    * Modified page
    
    Co-authored-by: Ephraim Bundi <ebundi04@gmail.com>
    Co-authored-by: Sir-George <86794695+Sir-George@users.noreply.github.com>
    
    * Feature/country page (#11)
    
    * initial commit
    
    * adding images to the images folder
    
    * renaming the file
    
    * styling the inserted elements
    
    * adding a description to the first container about the tours
    
    * filling in details for the other countries
    
    * adding a page for maldives
    
    * adding a page for france
    
    * adding a page for egypt
    
    * adding a stylesheet
    
    * adding a stylesheet
    
    * adding a page for dubai
    
    * editing destination info
    
    * adding the ticketing link and price estimates
    
    * adding the ticketing link and price estimates
    
    * formatting the documents
    
    * Create help.md
    
    * test
    
    * test
    
    * adding a nav bar and editing some of the page elements
    
    * Feature/country page (#12)
    
    * initial commit
    
    * adding images to the images folder
    
    * renaming the file
    
    * styling the inserted elements
    
    * adding a description to the first container about the tours
    
    * filling in details for the other countries
    
    * adding a page for maldives
    
    * adding a page for france
    
    * adding a page for egypt
    
    * adding a stylesheet
    
    * adding a stylesheet
    
    * adding a page for dubai
    
    * editing destination info
    
    * adding the ticketing link and price estimates
    
    * adding the ticketing link and price estimates
    
    * formatting the documents
    
    * Create help.md
    
    * test
    
    * test
    
    * adding a nav bar and editing some of the page elements
    
    * removin a shadow file
    
    * adding a licence and a read me file
    
    * editing the anchor tag for booking
    
    * Feature/country page (#13)
    
    * initial commit
    
    * adding images to the images folder
    
    * renaming the file
    
    * styling the inserted elements
    
    * adding a description to the first container about the tours
    
    * filling in details for the other countries
    
    * adding a page for maldives
    
    * adding a page for france
    
    * adding a page for egypt
    
    * adding a stylesheet
    
    * adding a stylesheet
    
    * adding a page for dubai
    
    * editing destination info
    
    * adding the ticketing link and price estimates
    
    * adding the ticketing link and price estimates
    
    * formatting the documents
    
    * Create help.md
    
    * test
    
    * test
    
    * adding a nav bar and editing some of the page elements
    
    * removin a shadow file
    
    * adding a licence and a read me file
    
    * editing the anchor tag for booking
    
    * adding a footer
    
    * Ft homepage (#10)
    
    * Initial commit
    
    * Add header and paragraph
    
    * Style homepage
    
    * Add new page
    
    * Add images
    
    * Add logo
    
    * Add navbar and countries section
    
    * Style homepage:
    
    * Modify Homepage
    
    * Add footer and contact section
    
    * Initial commit
    
    * Modified page
    
    * Modified html page
    
    * Modify html page
    
    Co-authored-by: Ephraim Bundi <ebundi04@gmail.com>
    Co-authored-by: Sir-George <86794695+Sir-George@users.noreply.github.com>
    Co-authored-by: Cherryl Osewe <86954916+CherrylOsewe@users.noreply.github.com>
    
    * unifying everything
    
    Co-authored-by: Ephrao1 <86771429+Ephrao1@users.noreply.github.com>
    Co-authored-by: Ephraim Bundi <ebundi04@gmail.com>
    Co-authored-by: Sir-George <86794695+Sir-George@users.noreply.github.com>
    Co-authored-by: Cherryl Osewe <86954916+CherrylOsewe@users.noreply.github.com>
    Co-authored-by: Cherryl Osewe <cherrylwendy03@gmail.com>

---
+++ js/homepage.js
@@ -0,0 +1,13 @@
+
+function myFunction() { 
+let inputmessage = document.getElementById("message").value
+let intmessage = parseInt(inputmessage)
+
+
+let inputfName = document.getElementById("fname").value
+let intfName = parseInt(inputfName)
+
+let inputlName = document.getElementById("lname").value
+let intlName = parseInt(inputlName)
+    alert("Message recieved.We will get back to you as soon as possible")
+  }
